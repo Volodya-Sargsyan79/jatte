23144da826bafbcacae22aa7f14a5c3abb996c15
Volodya Sargsyan <volsarg@gmail.com>
2023-08-28 13:27:47 +0400

    add and edit users

--- chat/views.py
+++ chat/views.py
@@ -47,21 +47,55 @@ def room(request, uuid):
     })
 
 @login_required
+def user_detail(request, uuid):
+    user = User.objects.get(pk=uuid)
+    rooms = user.rooms.all()
+    return render(request, 'chat/user_detail.html', {
+        'user': user,
+        'rooms': rooms
+    })
+
+
+@login_required
+def edit_user(request, uuid):
+    if request.user.has_perm('user.edit_user'):
+        user = User.objects.get(pk=uuid)
+
+        if request.method == 'POST':
+            form = EditUserForm(request.POST, instance=user)
+
+            if form.is_valid():
+                form.save()
+                
+                messages.success(request, 'The changes was seved!')
+
+                return redirect('/chat-admin/')
+        else:
+            form = EditUserForm(instance=user)
+        return render(request, 'chat/edit_user.html', {
+            'user': user,
+            'form': form
+        })
+    else:
+        messages.error(request, 'You don\'t have access to add users!')
+
+        return redirect('/chat-admin/')
+
+
+@login_required
 def add_user(request):
     if request.user.has_perm('user.add_user'):
         if request.method == 'POST':
             form = AddUserForm(request.POST)
-
             if form.is_valid():
                 user = form.save(commit=False)
                 user.is_staff = True
                 user.set_password(request.POST.get('password'))
                 user.save()
-
                 if user.role == User.MANAGER:
                     group = Group.objects.get(name='Managers')
                     group.user_set.add(user)
-                
+
                 messages.success(request, 'The user was added!')
 
                 return redirect('/chat-admin/')
